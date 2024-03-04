6646dd54ccccf98bc221a2a1193c8fc10b064467
julian carreno <julianctsistemas@gmail.com>
2024-03-04 09:35:29 -0500

    login

--- lib/screens/home_login.dart
+++ lib/screens/home_login.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:consumir_api/screens/admin_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -40,36 +39,34 @@ class _LoginScreenState extends State<LoginScreen> {
       final pass = responseData['password'];
       final message = responseData['message'];
 
-    
-        if (password == pass) {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            backgroundColor: Color.fromARGB(255, 197, 8, 8),
-            content: const Text('Email o contraseña incorrectas'),
+      if (password == pass) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          backgroundColor: Color.fromARGB(255, 197, 8, 8),
+          content: const Text('Email o contraseña incorrectas'),
+          duration: const Duration(seconds: 1),
+        ));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Color.fromRGBO(102, 51, 153, 1),
+            content: const Text('Bienvenido'),
             duration: const Duration(seconds: 1),
-          ));
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              backgroundColor: Color.fromRGBO(102, 51, 153, 1),
-              content: const Text('Bienvenido'),
-              duration: const Duration(seconds: 1),
-            ),
-          );
+          ),
+        );
 
-          // Save the token locally
-          final token = responseData['token'];
-          final prefs = await SharedPreferences.getInstance();
-          prefs.setString('token', token);
+        // Save the token locally
+        final token = responseData['token'];
+        final prefs = await SharedPreferences.getInstance();
+        prefs.setString('token', token);
 
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const AdminScreen()),
-          );
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const AdminScreen()),
+        );
 
-          emailController.clear();
-          passwordController.clear();
-        }
-      
+        emailController.clear();
+        passwordController.clear();
+      }
     } else if (response.statusCode == 401) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         backgroundColor: Color.fromARGB(255, 197, 8, 8),
@@ -82,110 +79,178 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 100,
-              ),
-              SizedBox(
-                width: 300,
-                height: 200,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(50.0),
-                  child: Image.asset(
-                    'assets/img/logo.png',
-                    fit: BoxFit.cover,
-                    color: null,
-                    
-                  ),
-                ),
-              ),
-              const SizedBox(height: 50),
-              SizedBox(
-                width: 325.0,
-                child: TextFormField(
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    labelText: "Correo electrónico",
-                    hintText: "Ingrese el correo electrónico",
-                    prefixIcon: Icon(Icons.email_outlined),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor, ingrese el correo electrónico';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              SizedBox(
-                width: 325.0,
-                child: TextFormField(
-                  controller: passwordController,
-                  obscureText: _isVisible,
-                  decoration: InputDecoration(
-                    labelText: "Contraseña",
-                    hintText: "Ingrese la contraseña",
-                    prefixIcon: const Icon(Icons.lock),
-                    border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                    ),
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _isVisible = !_isVisible;
-                        });
-                      },
-                      icon: _isVisible
-                          ? const Icon(Icons.visibility)
-                          : const Icon(Icons.visibility_off),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor, ingrese la contraseña';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    apiLogin();
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: const Color.fromRGBO(102, 51, 153, 1),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/img/fondo5.jpg"), // Ruta de tu imagen de fondo
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 400),
+            padding: const EdgeInsets.all(50.0),
+            child: SingleChildScrollView(
+              child: Center(
                 child: Container(
-                  width: 150.0,
-                  padding: const EdgeInsets.all(8.0),
-                  child: const Center(
-                    child: Text(
-                      'Iniciar sesión',
-                      style: TextStyle(
-                        color: Color.fromRGBO(235, 234, 236, 1),
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15.0),
+                    border: Border.all(
+                      color: Colors.grey.withOpacity(0.5),
+                      width: 2.0,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
                       ),
+                    ],
+                  ),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 250,
+                          height: 200,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(50.0),
+                            child: Image.asset(
+                              'assets/img/logo.png',
+                              fit: BoxFit.cover,
+                              color: null,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 0),
+                        SizedBox(
+                            width: 325.0,
+                            child: TextFormField(
+                              controller: emailController,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                labelText: "Correo electrónico",
+                                hintText: "Ingrese el correo electrónico",
+                                prefixIcon: Icon(Icons.email_outlined),
+                                enabledBorder: const OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                  borderSide: BorderSide(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                  borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor, // Color cuando el campo está activo
+                                  ),
+                                ),
+                                errorBorder: const OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                  borderSide: BorderSide(
+                                    color: Colors.red, // Color cuando hay un error de validación
+                                  ),
+                                ),
+                                border: const OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                  borderSide: BorderSide(
+                                    color: Colors.grey, // Color de borde predeterminado
+                                  ),
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Por favor, ingrese el correo electrónico';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+
+                        const SizedBox(height: 20.0),
+                        SizedBox(
+                              width: 325.0,
+                              child: TextFormField(
+                                controller: passwordController,
+                                obscureText: _isVisible,
+                                decoration: InputDecoration(
+                                  labelText: "Contraseña",
+                                  hintText: "Ingrese la contraseña",
+                                  prefixIcon: const Icon(Icons.lock),
+                                  enabledBorder: const OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                    borderSide: BorderSide(color: Colors.grey),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                  borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor,
+                                ),
+                                ),
+                                errorBorder: const OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                  borderSide: BorderSide(color: Colors.red),
+                                ),
+                                border: const OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                                  borderSide: BorderSide(color: Colors.grey),
+                                ),
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _isVisible = !_isVisible;
+                                    });
+                                  },
+                                  icon: _isVisible
+                                      ? const Icon(Icons.visibility)
+                                      : const Icon(Icons.visibility_off),
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Por favor, ingrese la contraseña';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              apiLogin();
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromRGBO(102, 51, 153, 1),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                          child: Container(
+                            width: 150.0,
+                            padding: const EdgeInsets.all(8.0),
+                            child: const Center(
+                              child: Text(
+                                'Iniciar sesión',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(235, 234, 236, 1),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-            ],
+            ),
           ),
         ),
       ),
